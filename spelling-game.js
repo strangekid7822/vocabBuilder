34e90986ef652fd2491667bace6f3de8c8d09274
strangekid7822 <34265324+strangekid7822@users.noreply.github.com>
2023-07-09 23:48:51 +0800

    Create a spelling game page

--- spelling-game.js
+++ spelling-game.js
@@ -1,25 +1,26 @@
+window.onload = function() {
+  fetchWords();
+  document.getElementById('word-input').value = ''; // Ensure the input field is empty when the page loads
+}
+
 let wordList = [];
+let currentWord;
+let submitButton = document.getElementById('submit-answer');
+let inputField = document.getElementById('word-input');
+let correctAnswerDiv = document.getElementById('correct-answer');
 
 async function fetchWords() {
   try {
     const response = await fetch('/api/words');
     const data = await response.json();
     wordList = data;
-    if (wordList.length < 6) {
-      document.getElementById('game-container').style.display = 'none'; // Hide game container
-      let warningMessage = document.createElement('h3');
-      warningMessage.textContent = "To play the game, please save more than 6 words.";
-      warningMessage.style.textAlign = 'center';
-      document.querySelector('.container').appendChild(warningMessage);
-    } else {
+    if (wordList.length > 0) {
       pickRandomWord();
     }
   } catch (error) {
     console.error('Error:', error);
   }
 }
-
-let currentWord;
 
 function pickRandomWord() {
   let randomIndex = Math.floor(Math.random() * wordList.length);
@@ -33,8 +34,40 @@ function displayWordInfo() {
   document.getElementById('word-chinese').textContent = currentWord.chinese_translation;
 }
 
-window.onload = function() {
-  console.log("Spelling game JS is linked correctly!");
-  document.getElementById('word-input').focus();  // Automatically focus the input field
-  fetchWords();  // Call fetchWords here
+submitButton.addEventListener('click', function(e) {
+  e.preventDefault();  // prevent form submission
+  if (submitButton.textContent === 'Submit') {
+    if (inputField.value === '') {
+      alert('You have to input the answer before submitting.');
+    } else if (inputField.value === currentWord.word) {
+      // Correct answer
+      inputField.style.backgroundColor = 'lightgreen';
+      displayCorrectAnswer();
+      inputField.classList.add('no-hover'); // Add the no-hover class when the input is disabled
+    } else {
+      // Incorrect answer
+      inputField.style.backgroundColor = 'lightpink';
+      displayIncorrectAnswer();
+      inputField.classList.add('no-hover'); // Add the no-hover class when the input is disabled
+    }
+    inputField.disabled = true;
+    submitButton.textContent = 'Next';
+  } else {
+    // Reset for next word
+    inputField.value = '';
+    inputField.style.backgroundColor = '';
+    inputField.disabled = false;
+    inputField.classList.remove('no-hover'); // Remove the no-hover class when the input is enabled
+    correctAnswerDiv.innerHTML = '';  // clear the correct answer div
+    submitButton.textContent = 'Submit';
+    pickRandomWord();
+  }
+});
+
+function displayCorrectAnswer() {
+  correctAnswerDiv.innerHTML = "<span style='color: gray;'>Correct answer!</span>";
+}
+
+function displayIncorrectAnswer() {
+  correctAnswerDiv.innerHTML = "<span style='color: gray;'>Incorrect. The correct answer was: </span><span style='color: red; font-weight: bold;'>" + currentWord.word + "</span>";
 }
