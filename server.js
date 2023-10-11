784d084e139d717492a02fae6c307c4a2ac99a90
strangekids7822 <keji_zhaoqiang2006@163.com>
2023-10-11 09:10:43 +0800

    update the server.js to switch to mo ngodb

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const sqlite3 = require('sqlite3').verbose();
+// Import MongoDB client
+const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
 
 console.log('Starting server.js...');
@@ -8,30 +9,36 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const db = new sqlite3.Database('./words.db', (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the SQLite database.');
+// Declare a variable to hold the MongoDB database object
+let db;
+
+// Connect to MongoDB
+MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true }, (err, client) => {
+  if (err) return console.error(err);
+  console.log('Connected to MongoDB');
+  // Initialize the database object
+  db = client.db('vocabBuilder');
 });
 
-db.serialize(() => {
-  db.run('CREATE TABLE IF NOT EXISTS words (id INTEGER PRIMARY KEY, word TEXT NOT NULL, meaning TEXT, synonyms TEXT, chinese_translation TEXT);');
-});
-
+// POST route for adding words
 app.post('/api/words', (req, res) => {
   const { word, meaning, synonyms, chinese_translation } = req.body;
-  db.run('INSERT INTO words (word, meaning, synonyms, chinese_translation) VALUES (?, ?, ?, ?)', [word, meaning, synonyms, chinese_translation], function (err) {
+  // Use MongoDB collection instead of SQLite
+  const collection = db.collection('words');
+  collection.insertOne({ word, meaning, synonyms, chinese_translation }, (err, result) => {
     if (err) {
       res.status(500).json({ error: err.message });
       return;
     }
-    res.json({ id: this.lastID });
+    res.json({ id: result.insertedId });
   });
 });
 
+// GET route for fetching words
 app.get('/api/words', (req, res) => {
-  db.all('SELECT * FROM words ORDER BY word ASC', [], (err, rows) => {
+  // Use MongoDB collection instead of SQLite
+  const collection = db.collection('words');
+  collection.find({}).toArray((err, rows) => {
     if (err) {
       res.status(500).json({ error: err.message });
       return;
@@ -40,34 +47,38 @@ app.get('/api/words', (req, res) => {
   });
 });
 
+// PUT route for updating words
 app.put('/api/words/:id', (req, res) => {
   const { id } = req.params;
   const { word, meaning, synonyms, chinese_translation } = req.body;
-
-  db.run('UPDATE words SET word = ?, meaning = ?, synonyms = ?, chinese_translation = ? WHERE id = ?', [word, meaning, synonyms, chinese_translation, id], function (err) {
+  // Use MongoDB collection instead of SQLite
+  const collection = db.collection('words');
+  collection.updateOne({ _id: id }, { $set: { word, meaning, synonyms, chinese_translation } }, (err, result) => {
     if (err) {
       res.status(500).json({ error: err.message });
       return;
     }
-    res.json({ changes: this.changes });
+    res.json({ changes: result.modifiedCount });
   });
 });
 
+// DELETE route for deleting words
 app.delete('/api/words/:id', (req, res) => {
   const { id } = req.params;
-
-  db.run('DELETE FROM words WHERE id = ?', [id], function (err) {
+  // Use MongoDB collection instead of SQLite
+  const collection = db.collection('words');
+  collection.deleteOne({ _id: id }, (err, result) => {
     if (err) {
       res.status(500).json({ error: err.message });
       return;
     }
-    res.json({ changes: this.changes });
+    res.json({ changes: result.deletedCount });
   });
 });
 
 app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 3000;
-app.listen(3000, '0.0.0.0', () => {
-  console.log('Server is running on port 3000');
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
 });
