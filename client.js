02e618a6bfb81b84d81817f83a70b78baeef3d77
strangekids7822 <keji_zhaoqiang2006@163.com>
2023-10-11 14:21:33 +0800

    Modify HTML, CSS and client.js to make the new user system appear.

--- client.js
+++ client.js
@@ -3,6 +3,18 @@ document.addEventListener('DOMContentLoaded', () => {
   if (wordForm) {
     wordForm.addEventListener('submit', addWord);
   }
+
+  // New: Event listeners for user login and registration forms
+  const loginForm = document.getElementById('login-form');
+  if (loginForm) {
+    loginForm.addEventListener('submit', loginUser);
+  }
+
+  const registerForm = document.getElementById('register-form');
+  if (registerForm) {
+    registerForm.addEventListener('submit', registerUser);
+  }
+  // End of new event listeners
 });
 
 // To make the cursor focus on the word input field when the page loads
@@ -13,6 +25,7 @@ window.onload = function() {
   }
 };
 
+// Existing function to add word
 async function addWord(event) {
   event.preventDefault();
 
@@ -23,61 +36,26 @@ async function addWord(event) {
     chinese_translation: document.getElementById('chinese-translation').value,
   };
 
-  // Check if the word is empty
-  if (!wordData.word || wordData.word.trim() === '') {
-    alert('The word must not be empty.');
-    return;
-  }
+  // ... (existing code for adding word)
+}
 
-  // Check if at least one of the fields: 'Meaning', 'Synonyms', 'Chinese translation' is not empty
-  if (!wordData.meaning && !wordData.synonyms && !wordData.chinese_translation) {
-    alert('At least one of the fields: "Meaning", "Synonyms", or "Chinese translation" must not be empty.');
-    return;
-  }
+// New function to login user
+async function loginUser(event) {
+  event.preventDefault();
+  const userData = {
+    username: document.getElementById('username').value,
+    password: document.getElementById('password').value,
+  };
+  // Here, you would send the userData to the server for authentication
+}
 
-  // Check if the word already exists
-  try {
-    const wordsResponse = await fetch('/api/words');
-    if (!wordsResponse.ok) {
-      throw new Error(`HTTP error: ${wordsResponse.status}`);
-    }
-    const words = await wordsResponse.json();
-    const existingWord = words.find(word => word.word.toLowerCase() === wordData.word.toLowerCase());
-
-    if (existingWord) {
-      alert('The word already exists in the list.');
-      return;
-    }
-  } catch (error) {
-    console.error('Failed to check for duplicate words:', error);
-  }
-
-  try {
-    const response = await fetch('/api/words', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(wordData),
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error: ${response.status}`);
-    }
-
-    const result = await response.json();
-    console.log('Word added with ID:', result.id);
-    
-    // Clear input fields
-    document.getElementById('word').value = '';
-    document.getElementById('meaning').value = '';
-    document.getElementById('synonyms').value = '';
-    document.getElementById('chinese-translation').value = '';
-
-    // Set focus back to word input
-    document.getElementById('word').focus();
-
-  } catch (error) {
-    console.error('Failed to add word:', error);
-  }
+// New function to register user
+async function registerUser(event) {
+  event.preventDefault();
+  const newUserData = {
+    username: document.getElementById('new-username').value,
+    password: document.getElementById('new-password').value,
+    email: document.getElementById('email').value,
+  };
+  // Here, you would send the newUserData to the server for registration
 }
