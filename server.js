f31a47d6da9610a2829a4d869a3058a85e8f5cd7
strangekids7822 <keji_zhaoqiang2006@163.com>
2023-10-12 15:20:14 +0800

    To make each user's word list unique, we'll need to modify the database schema to include a user identifier for each word.

--- server.js
+++ server.js
@@ -3,20 +3,12 @@ const MongoClient = require('mongodb').MongoClient;
 const cors = require('cors');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const session = require('express-session');  // New: Import express-session
 
 console.log('Starting server.js...');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-// New: Initialize session management
-app.use(session({
-  secret: 'your-secret-key',
-  resave: false,
-  saveUninitialized: true,
-}));
 
 let db;
 
@@ -26,9 +18,8 @@ MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true }, (
   db = client.db('vocabBuilder');
 });
 
-// Initialize Passport and session
+// Initialize Passport
 app.use(passport.initialize());
-app.use(passport.session());  // New: Enable session support in Passport
 
 // Passport local strategy for user authentication
 passport.use(new LocalStrategy(
@@ -42,19 +33,6 @@ passport.use(new LocalStrategy(
     });
   }
 ));
-
-// New: Serialize user information into session
-passport.serializeUser(function(user, done) {
-  done(null, user._id);
-});
-
-// New: Deserialize user information from session
-passport.deserializeUser(function(id, done) {
-  const collection = db.collection('users');
-  collection.findOne({ _id: id }, function(err, user) {
-    done(err, user);
-  });
-});
 
 // User registration route
 app.post('/api/register', (req, res) => {
@@ -78,6 +56,32 @@ app.post('/api/login', passport.authenticate('local', { failureRedirect: '/login
 app.get('/api/logout', (req, res) => {
   req.logout();
   res.redirect('/');
+});
+
+// Modified POST route for adding words to include user ID
+app.post('/api/words', (req, res) => {
+  const { word, meaning, synonyms, chinese_translation, userId } = req.body;
+  const collection = db.collection('words');
+  collection.insertOne({ word, meaning, synonyms, chinese_translation, userId }, (err, result) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({ id: result.insertedId });
+  });
+});
+
+// Modified GET route for fetching words to filter by user ID
+app.get('/api/words', (req, res) => {
+  const userId = req.query.userId; // Assuming userId is passed as a query parameter
+  const collection = db.collection('words');
+  collection.find({ userId }).toArray((err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json(rows);
+  });
 });
 
 app.use(express.static(__dirname));
